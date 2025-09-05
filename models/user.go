d78a95a58030b5c2ebdf574f5b3e8e8b396f07c4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-05 20:43:45 -0300

    Set user struct

--- models/user.go
+++ models/user.go
@@ -1,11 +1,17 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Username string             `json:"username" bson:"username"`
-	Email string             `json:"email" bson:"email"`
-	Password string             `json:"password" bson:"password"`
-	
+	ID           primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username     string             `json:"username" bson:"username"`
+	Email        string             `json:"email" bson:"email"`
+	Password     string             `json:"password" bson:"password"`
+	RefreshToken string             `json:"refresh_token" bson:"refresh_token"`
+	CreatedAt    *time.Time         `json:"created_at" bson:"created_at"`
+	UpdatedAt    *time.Time         `json:"updated_at" bson:"updated_at"`
 }
