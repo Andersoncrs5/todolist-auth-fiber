9f77872954efda9e7c353f4c00024afca9ada0ae
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-05 20:43:23 -0300

    new vars to entity Todo

--- models/todo.go
+++ models/todo.go
@@ -1,11 +1,17 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Todo struct {
 	ID     primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
 	Title  string             `json:"title" bson:"title"`
-	Discription  string        `json:"discription" bson:"discription"`
+	Discription  string       `json:"discription" bson:"discription"`
 	Done   bool               `json:"done" bson:"done"`
+	CreatedAt    *time.Time   `json:"created_at" bson:"created_at"`
+	UpdatedAt    *time.Time   `json:"updated_at" bson:"updated_at"`
 }
