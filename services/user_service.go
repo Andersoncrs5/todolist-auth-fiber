2ad55fd25311a77b8384523cc43aea35ed80ea8d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-06 11:20:31 -0300

    new method SetRefreshToken

--- services/user_service.go
+++ services/user_service.go
@@ -13,11 +13,12 @@ import (
 type UserService interface {
 	GetById(ctx context.Context, id primitive.ObjectID) (*models.User, error)
 	GetByEmail(ctx context.Context, email string) (*models.User, error)
-	Delete(ctx context.Context, user models.User) error
+	Delete(ctx context.Context, user *models.User) error
 	Save(ctx context.Context, dto userDto.CreateUserDTO) (*models.User, error)
 	ExistsByEmail(ctx context.Context, email string) (bool, error)
-	Update(ctx context.Context, user models.User, dto userDto.UpdateUserDTO) (*models.User, error)
+	Update(ctx context.Context, user *models.User, dto userDto.UpdateUserDTO) (*models.User, uint, error)
 	ExistsByUserName(ctx context.Context, UserName string) (bool, error)
+	SetRefreshToken(ctx context.Context, user *models.User, refreshToken string) (*models.User, int, error)
 }
 
 type userService struct {
@@ -56,7 +57,7 @@ func (u *userService) GetByEmail(ctx context.Context, email string) (*models.Use
 	return user, nil
 }
 
-func (u *userService) Delete(ctx context.Context, user models.User) error { 
+func (u *userService) Delete(ctx context.Context, user *models.User) error { 
 	err := u.repo.Delete(ctx, user.ID)
 	if err != nil {
 		return err
@@ -106,11 +107,20 @@ func (u *userService) ExistsByUserName(ctx context.Context, UserName string) (bo
 	return check, nil
 }
 
-func (u *userService) Update(ctx context.Context, user models.User, dto userDto.UpdateUserDTO) (*models.User, error) {
-	updated, err := u.repo.Update(ctx, user.ID, dto)
+func (u *userService) Update(ctx context.Context, user *models.User, dto userDto.UpdateUserDTO) (*models.User, uint, error) {
+	updated, code, err := u.repo.Update(ctx, user.ID, dto)
 	if err != nil {
-		return nil, err
+		return nil, code, err
 	}
 
-	return updated, nil
+	return updated, code, nil
+}
+
+func (u *userService) SetRefreshToken(ctx context.Context, user *models.User, refreshToken string) (*models.User, int, error) {
+	userSaved, code, err := u.repo.SetRefreshToken(ctx, user.ID, refreshToken)
+	if err != nil {
+		return nil, code, err
+	}
+
+	return userSaved, 200, nil
 }
