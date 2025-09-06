e9121b94c3422ff2f75b57e3de9102ed14dd9320
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-06 13:58:49 -0300

    Set fiber config

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"todolist-auth-fiber/config"
+	"todolist-auth-fiber/handlers"
+	repository "todolist-auth-fiber/repositories"
+	"todolist-auth-fiber/routers"
+	"todolist-auth-fiber/services"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
+	app := fiber.New()
+
+	config.ConnectDB()
+	db := config.GetDB()
 	
+	userRepository := repository.NewUserRepository(db)
+	userService := services.NewUserService(userRepository)
+	userHandler := handlers.NewUserHandler(userService)
+
+	routers.UserRouter(app, userHandler)
+
+	app.Listen(":8080")
 }
